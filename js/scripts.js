4e040bc4c284fded5e89c914eb22798b3eabc2ae
Aisha Sharif <sharif_aisha@yahoo.com>
2018-05-04 16:03:46 +0300

    create the user interface logic on script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ var pingPong = function(input) {
   var result = [];
   for(var i = 1; i <= input; i++){
      if (i % 3 === 0 && i % 5 === 0){
-      result.push('ping-pong');
+      result.push('pingpong');
     }else	if(i % 3 === 0){
       result.push('ping');
     } else if (i % 5 === 0){
@@ -14,3 +14,18 @@ var pingPong = function(input) {
   }
     return(result)
 };
+
+//UI Logic
+$(document).ready(function(){
+	$('#myForm').submit(function(e){
+    $("#output").empty();
+  	e.preventDefault();
+    var input = parseInt($('input#value').val());
+  	var output = pingPong(input);
+    
+    output.forEach(function(item) {
+    $("#output").append('<li>' + item + '</li>');
+    });
+    /* $('#value').val('') */
+  });
+});
